15f12c97ddade20acad07544b54c2acbda958d12
Miguel Ramos <return.infox@gmail.com>
2023-09-05 20:11:28 -0300

    creando el framework de testing

--- app/src/test/java/Steps/APISteps.java
+++ app/src/test/java/Steps/APISteps.java
@@ -8,6 +8,9 @@ import io.restassured.response.ValidatableResponse;
 import io.restassured.specification.RequestSpecification;
 
 import static io.restassured.RestAssured.given;
+import static org.junit.Assert.*;
+
+import java.util.List;
 public class APISteps {
 
   //* para configurar los detalles de la solicitud
@@ -29,4 +32,30 @@ public class APISteps {
     response = request.when().get("https://jsonplaceholder.typicode.com/posts");
     json = response.then().statusCode(expectedStatusCode);
   }
+
+  //* Método para verificar el número de elementos de una respuesta
+  @Then("^I validate there are (\\d+) items on the (.+) endpoint$")
+  public void validateSize(int expectedSize, String endpoint) {
+
+    response = request.when()
+               .get(endpoint);
+
+    //* vamos a extraer de la respuesta
+    List<String> jsonResponse = response.jsonPath().getList("$");
+    int actualSize = jsonResponse.size();
+
+    //* Afirmar que el tamaño esperado es igual al tamaño real
+    // * el valor esperado y el valor real recibido del request
+    assertEquals(expectedSize, actualSize);
+
+  }
+
+  //* Método para verificar un valor especifico en la respuesta
+  @Then("^Valido que hay un valor: (.+) en la respuesta en el endpoint (.+)$")
+  public void validateValue(String expectedValue, String endpoint) {
+    response = request.when().get(endpoint);
+    List<String> jsonResponse = response.jsonPath().getList("username");
+    assertTrue("El valor " + expectedValue + " no se encuentra en la lista", jsonResponse.contains(expectedValue));
+  }
+
 }
--- app/src/test/resources/features/APITest.feature
+++ app/src/test/resources/features/APITest.feature
@@ -1,5 +1,15 @@
 Feature: Ejemplo de Request
-  @API
+
   Scenario: Prueba Get al endpoint
     Given Envio una peticion Get a la URI https://jsonplaceholder.typicode.com/posts
     Then Recibo un codigo de estado 200
+
+ 
+  Scenario: Validar la cantidad de entradas en la respuesta
+    Given Envio una peticion Get a la URI https://jsonplaceholder.typicode.com
+    Then I validate there are 100 items on the /albums endpoint
+
+  @API
+  Scenario: Validar que un elemento está en la respuesta
+     Given Envio una peticion Get a la URI https://jsonplaceholder.typicode.com
+     Then Valido que hay un valor: Kamren en la respuesta en el endpoint /users
